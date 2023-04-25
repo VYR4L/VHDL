4c9f8d84417f8a9f9cb6a0b6dd7623922acb2945
Felipe Kravec Zanatta <118224396+VYR4L@users.noreply.github.com>
2023-04-24 22:18:09 -0300

    Update contador.vhdl

--- Contador/contador.vhdl
+++ Contador/contador.vhdl
@@ -3,9 +3,9 @@ use ieee.std_logic_1164.all;
 
 entity contador is
     port(
-        clock : in std_logic;
+        clock_c : in std_logic;
         cl : in std_logic;
-        q : out std_logic_vector(2 downto 0)
+        q_c : out std_logic_vector(2 downto 0)
 );
 end entity;
 
@@ -26,16 +26,17 @@ architecture contar of contador is
     );
     end component;
 
-    signal s_j, s_k, s_q     : std_logic_vector(2 downto 0);
-    signal s_clock, s_pr, s_cl   : std_logic;
+    signal s_j, s_k, s_q, s_nq   : std_logic_vector(2 downto 0);
+    signal vcc : std_logic := '1';
 
     begin
+        u_ctrl  : ctrl port map(s_q, s_j, s_k);
         
-        
-        u_ffjk2 : ffjk port map(s_j(2), s_k(2),s_clock, s_pr, s_cl,  s_q(2));
-        u_ffjk1 : ffjk port map(s_j(1), s_k(1),s_clock, s_pr, s_cl,  s_q(1));
-        u_ffjk0 : ffjk port map(s_j(0), s_k(0),s_clock, s_pr, s_cl,  s_q(0));
+        u_ffjk2 : ffjk port map(s_j(2), s_k(2),clock_c, vcc, cl,  s_q(2), s_nq(2));
+        u_ffjk1 : ffjk port map(s_j(1), s_k(1),clock_c, vcc, cl,  s_q(1), s_nq(1));
+        u_ffjk0 : ffjk port map(s_j(0), s_k(0),clock_c, cl, vcc,  s_q(0), s_nq(0));
 
-        u_ctrl  : ctrl port map(s_j(2 downto 0), s_k(2 downto 0), s_q(2 downto 0));
+       
 
+        q_c <= s_q;
 end architecture;
