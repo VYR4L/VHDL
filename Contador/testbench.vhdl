b8c4e20461c91ada2cf5f6a717152213e7ddc6c6
Felipe Kravec Zanatta <118224396+VYR4L@users.noreply.github.com>
2023-04-24 22:18:52 -0300

    Update testbench.vhdl

--- Contador/testbench.vhdl
+++ Contador/testbench.vhdl
@@ -1,34 +1,46 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity tb is
+entity testbench is
 end entity;
 
 architecture teste of testbench is
     constant CLK_PERIOD : time := 20 ns;
 
-    component ffjk is
-        port(        
-            j, k   : in  std_logic;        
-            clock  : in  std_logic;        
-            pr, cl : in  std_logic;        
-            q, nq  : out std_logic    
-        );    
-    end component;
-
-    component ctrl is
-        port(
-            q : in std_logic_vector(2 downto 0);
-            j, k : out std_logic_vector(2 downto 0)
-        );
-    end component;
-
     component contador is
         port(
-            clock : in std_logic;
+            clock_c : in std_logic;
             cl : in std_logic;
-            q : out std_logic_vector(2 downto 0)
+            q_c : out std_logic_vector(2 downto 0)
         );
     end component;
 
-end entity;
+    signal s_clock : std_logic := '1';
+    signal s_cl : std_logic;
+    signal s_q_c : std_logic_vector(2 downto 0);
+begin
+    u_contador : contador port map(s_clock, s_cl, s_q_c);
+
+    u_teste: process
+    begin
+        s_cl <= '0';
+        wait for CLK_PERIOD;
+        s_cl <= '1';
+        wait for CLK_PERIOD;
+        wait for CLK_PERIOD;
+        wait for CLK_PERIOD;
+        wait for CLK_PERIOD;
+        wait for CLK_PERIOD;
+        wait for CLK_PERIOD;
+        wait for CLK_PERIOD;
+        wait for CLK_PERIOD;
+        wait for CLK_PERIOD;
+    end process;
+
+    u_clock : process
+    begin
+        s_clock <= not(s_clock);
+        wait for CLK_PERIOD/2;
+    end process;
+
+end architecture;
